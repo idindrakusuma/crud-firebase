bdb820fa50bdcd24b5fcc672ca215a1b3ff6b94d
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-02-03 23:08:29 +0700

    init awal

---
+++ app/src/main/java/com/nbs/apps/gojekclone/Order.java
@@ -0,0 +1,38 @@
+package com.nbs.apps.gojekclone;
+
+/**
+ * Created by acer on 03/02/2017.
+ * INDRA KUSUMA
+ */
+
+public class Order {
+
+    public String dari;
+    public String ke;
+    //tambahan
+    public String jarak;
+
+    public String getDari() {
+        return dari;
+    }
+
+    public void setDari(String dari) {
+        this.dari = dari;
+    }
+
+    public String getKe() {
+        return ke;
+    }
+
+    public void setKe(String ke) {
+        this.ke = ke;
+    }
+
+    public String getJarak() {
+        return jarak;
+    }
+
+    public void setJarak(String jarak) {
+        this.jarak = jarak;
+    }
+}
